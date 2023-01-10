5b0cdd75581f97e798676f209a91d7f4563ed33b
Thomas_Cardoso <tcardo06@gmail.com>
2023-01-10 17:02:17 +0100

    add parallax background

---
+++ Assets/Script/Parallax.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    private float lenght, starpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        starpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect)); ;
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(starpos + dist, transform.position.y, transform.position.z);
+
+        if (temp > starpos + lenght) starpos += lenght;
+        else if (temp < starpos - lenght) starpos -= lenght;
+    }
+}
