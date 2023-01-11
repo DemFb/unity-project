d121b7bcca066646d248f6c1da4aa04de2072256
D.Fab <dembele.fabala@gmail.com>
2023-01-11 09:18:53 +0100

    feat: add kill nounours

---
+++ Assets/Script/Enemy/WeakSpot.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeakSpot : MonoBehaviour
+{
+
+    public GameObject objectToDestroy;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Destroy(objectToDestroy);
+        }
+    }
+}
