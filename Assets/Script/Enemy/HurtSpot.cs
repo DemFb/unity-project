8d1e9ba8b25e16d36f6ebe86b5520798f10683bb
D.Fab <dembele.fabala@gmail.com>
2023-01-13 03:16:31 +0100

    add damage death zone and fill heart / add heart

--- Assets/Script/Enemy/HurtSpot.cs
+++ Assets/Script/Enemy/HurtSpot.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class HurtSpot : MonoBehaviour
 {
     public float dmg;
-  //  public float knockbackForce;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
@@ -17,41 +16,3 @@ public class HurtSpot : MonoBehaviour
     }
 }
 
-
-
-/*
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HurtSpot : MonoBehaviour
-{
-    public float dmg;
-    public Rigidbody2D enmeyRb;
-    public float knockbackForce;
-
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.otherCollider.CompareTag("Player"))
-        {
-            PlayerStats.Instance.TakeDamage(dmg);
-            Vector2 difference = (transform.position - col.transform.position).normalized;
-            Vector2 force = difference * knockbackForce;
-            enmeyRb.AddForce(force, ForceMode2D.Impulse); 
-        }
-    }
-
-    /*private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Player"))
-        {
-            PlayerStats.Instance.TakeDamage(dmg);
-            Vector2 difference = (transform.position - col.transform.position).normalized;
-            Vector2 force = difference * knockbackForce;
-            enmeyRb.AddForce(force, ForceMode2D.Impulse);
-        }
-    }#1#
-}
-*/
